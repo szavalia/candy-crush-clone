a767a5efa356fe3c19ad163b6b125324870de626
Julian <juliansicardi@gmail.com>
2019-12-05 18:30:45 -0300

    Cambios Generales - agrego getAux y variable aux a GameState

--- src/game/backend/GameState.java
+++ src/game/backend/GameState.java
@@ -1,28 +1,37 @@
 package game.backend;
 
 public abstract class GameState {
-	
+
 	private long score = 0;
 	private int moves = 0;
-	
+	private int aux = 0;
+
 	public void addScore(long value) {
 		this.score = this.score + value;
 	}
-	
+
 	public long getScore(){
 		return score;
 	}
-	
+
 	public void addMove() {
 		moves++;
 	}
-	
+
 	public int getMoves() {
 		return moves;
 	}
-	
+
 	public abstract boolean gameOver();
-	
+
 	public abstract boolean playerWon();
+
+	public int getAux(){
+		return aux;
+	}
+
+	public void addAux(int value){
+		aux+=value;
+	}
 
 }

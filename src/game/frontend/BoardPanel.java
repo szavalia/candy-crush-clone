b15dc7449793ddc5b01f3c6ee8df452d5f7fbf07
Julian <juliansicardi@gmail.com>
2019-12-06 14:56:46 -0300

    Cambios Generales - boardPanel mio y de valchar fusionados

--- src/game/frontend/BoardPanel.java
+++ src/game/frontend/BoardPanel.java
@@ -4,34 +4,50 @@ import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 
 public class BoardPanel extends TilePane {
 
-	private ImageView[][] cells;
+	private StackPane[][] cells;
 
 	public BoardPanel(final int rows, final int columns, final int cellSize) {
 		setPrefRows(rows);
 		setPrefColumns(columns);
 		setPrefTileHeight(cellSize);
 		setPrefTileWidth(cellSize);
-		this.cells = new ImageView[rows][columns];
+		this.cells = new StackPane[rows][columns];
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				cells[i][j] = new ImageView();
+				cells[i][j] = new StackPane();
 				getChildren().add(cells[i][j]);
 			}
 		}
 	}
-	
+
 	public void setImage(int row, int column, Image image) {
-		cells[row][column].setImage(image);
+		cells[row][column].getChildren().add(new ImageView(image));
+		cells[row][column].setEffect(null);
+	}
+
+	public void setWallImage(int row, int column, Image image) {
+		setImage( row , column , image );
+		Light.Distant spotLight = new Light.Distant();
+		spotLight.setColor(Color.SANDYBROWN);
+		spotLight.setElevation(100);
+		Lighting lighting = new Lighting(spotLight);
+		cells[row][column].setEffect(lighting);
+	}
+
+	public void setJailImage( int row , int column , Image image , Image Jail){
+		setImage(row , column , image );
+		cells[row][column].getChildren().add(new ImageView(Jail));
 
 	}
 
 	public void setGoldenImage( int row, int column, Image image){
-		cells[row][column].setImage(image);
+		setImage(row, column , image);
 		Light.Distant spotLight = new Light.Distant();
 		spotLight.setColor(Color.YELLOW);
 		spotLight.setElevation(100);

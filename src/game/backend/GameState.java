bd902d7740a3720ec7c6d7c3d857ee6d92a60587
szavalia <szavalia@itba.edu.ar>
2019-12-06 19:57:47 -0300

    Eliminando warnings y mejorando estilo

--- src/game/backend/GameState.java
+++ src/game/backend/GameState.java
@@ -34,4 +34,6 @@ public abstract class GameState {
 		aux = value;
 	}
 
+	public void updateAux(){};
+
 }
--- src/game/backend/level/Level4.java
+++ src/game/backend/level/Level4.java
@@ -1,39 +1,22 @@
 package game.backend.level;
 
 import game.backend.GameState;
-import game.backend.cell.Cell;
-import game.backend.element.BreakableElement;
 import game.backend.element.BreakableWall;
 
 public class Level4 extends Level {
-    private static int MAXMOVES = 500;
+    private final int MAXMOVES = 20;
     private static int WALLHEIGHT = 3;
     private static int WALLWIDTH = 3;
     private static int WALLCENTER = SIZE/2;
-    private Cell wallCell;
-    private Cell candyGenCell;
     @Override
     protected GameState newState(){ return new Level4State( MAXMOVES , WALLHEIGHT * WALLWIDTH);}
 
     @Override
-    public void initialize(){ ;
+    public void initialize(){
         super.initialize();
         addBreakableWalls();
-        checkWalls();
+        state().updateAux();
         wasUpdated();
-    }
-
-    public void checkWalls() {
-        int i, j, total = 0;
-        for (i = 0; i < WALLHEIGHT; i++) {
-            for (j = 0; j < WALLWIDTH; j++) {
-                if (g()[WALLCENTER - (WALLHEIGHT) / 2 + i][WALLCENTER - (WALLWIDTH) / 2 + j].getContent().isBreakable()) {
-                    total++;
-                }
-            }
-        }
-        state().setAux(total);
-        System.out.println( "Vi totales");
     }
 
     @Override
@@ -41,12 +24,12 @@ public class Level4 extends Level {
         boolean ret;
         if (ret = super.tryMove(i1, j1, i2, j2)) {
             state().addMove();
-            checkWalls();
+            state().updateAux();
         }
         return ret;
     }
 
-    public void addBreakableWalls() {
+    private void addBreakableWalls() {
         int i, j;
         for (i = 0; i < WALLHEIGHT; i++) {
             for (j = 0; j < WALLWIDTH; j++) {
@@ -59,9 +42,22 @@ public class Level4 extends Level {
 
         private long maxMoves;
 
-        public Level4State(int maxMoves, int walls ) {
+        private Level4State(int maxMoves, int walls ) {
             this.maxMoves = maxMoves;
             aux = walls;
+        }
+
+        @Override
+        public void updateAux() {
+            int i, j, total = 0;
+            for (i = 0; i < WALLHEIGHT; i++) {
+                for (j = 0; j < WALLWIDTH; j++) {
+                    if (g()[WALLCENTER - (WALLHEIGHT) / 2 + i][WALLCENTER - (WALLWIDTH) / 2 + j].getContent().isBreakable()) {
+                        total++;
+                    }
+                }
+            }
+            aux = total;
         }
 
         public boolean gameOver() {

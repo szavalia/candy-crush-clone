191efc2bead5f8bbe3167599f8b8329d1ba7668e
vriera <57446063+vriera@users.noreply.github.com>
2019-12-07 18:33:21 -0300

    Ultima version skere

--- src/game/frontend/CandyFrame.java
+++ src/game/frontend/CandyFrame.java
@@ -92,9 +92,9 @@ public class CandyFrame extends VBox {
 			} else {
 				Point2D newPoint = translateCoords(event.getX(), event.getY());
 				if (newPoint != null) {
-					System.out.println("Get second = " +  newPoint);
-					game().tryMove((int)lastPoint.getX(), (int)lastPoint.getY(), (int)newPoint.getX(), (int)newPoint.getY());
-					String message = ((Long)game().getScore()).toString();
+					System.out.println("Get second = " + newPoint);
+					game().tryMove((int) lastPoint.getX(), (int) lastPoint.getY(), (int) newPoint.getX(), (int) newPoint.getY());
+					String message = ((Long) game().getScore()).toString();
 					if (game().isFinished()) {
 						if (game().playerWon()) {
 							message = message + " Finished - Player Won! ";
@@ -102,7 +102,9 @@ public class CandyFrame extends VBox {
 							message = message + " Finished - Loser ! ";
 						}
 					}
-					scorePanel.updateScore("Score : " + message);
+					if (!game().isFinished()) {
+						scorePanel.updateScore("Score : " + message);
+					}
 					if(game().getMoves() >= 0) {
 						scorePanel.updateMove("Moves : " + Integer.toString(game.getMoves()));
 					}
